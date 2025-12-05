65033868347b80a002f1b3b156c3a8db08944bf2
kartik22gujar <kartik22gujar@gmail.com>
2025-12-05 18:40:24 +0530

    Day-03 folder

---
+++ Day-03/tryout.py
@@ -0,0 +1,16 @@
+#variable Global & local
+####################
+#global variables
+num1=10
+num2=20
+
+def adition():
+    #loacl variable 
+    num3=30
+    print(num1+num2+num3)
+
+def sub():
+    print(num1-num2)
+
+adition()
+sub()   

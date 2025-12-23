dddeb3abb04e42efc47b62ee461f6b3312a3f6db
kartik22gujar <kartik22gujar@gmail.com>
2025-12-23 19:15:47 +0530

    day06 added

---
+++ Day-06/assignment1.py
@@ -0,0 +1,16 @@
+num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+
+def add(num1,num2):
+    print("Dddition",num1+num2)
+def sub(num1,num2):
+    print("Substraction",num1-num2)
+def mul(num1,num2):
+    print("Multiplication",num1*num2)
+def div(num1,num2):
+    print("Division",num1/num2)    
+
+add(num1,num2)
+sub(num1,num2)
+mul(num1,num2)
+div(num1,num2)
---
+++ Day-06/assignment2.py
@@ -0,0 +1,16 @@
+num1 = int(input("enter 1st number:"))
+num2 = int(input("enter 2nd number:"))
+
+r1=num1<num2
+r2=num1>num2
+r3=num1<=num2
+r4=num1>=num2
+r5=num1==num2
+r6=num1!=num2
+
+print("result of <",r1)
+print("result of >",r2)
+print("result of <=",r3)
+print("result of >=",r4)
+print("result of ==",r5)
+print("result of !=",r6)

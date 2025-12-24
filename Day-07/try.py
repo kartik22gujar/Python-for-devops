aeef7e7db5bb8753e4c5e01d1262fb6382625f84
kartik22gujar <kartik22gujar@gmail.com>
2025-12-24 21:29:15 +0530

    folder update

---
+++ Day-07/try.py
@@ -0,0 +1,12 @@
+
+test= float(input())
+
+if test == 15:
+    print("number in equal to 15")
+
+elif test>15:
+    print("number is grater than 15")
+
+elif test<15:
+    print("number is less than 15")
+
